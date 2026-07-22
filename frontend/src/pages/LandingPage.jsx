bcc5a6dcc07ed67e4255cccceed523b80fed0ff7
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-22 21:02:52 +0530

    added hero section in the landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,10 @@
+import HeroSection from "../components/landing/HeroSection";
+
 function LandingPage() {
   return (
-    <section className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-semibold">LandingPage placeholder</h1>
-    </section>
+    <>
+      <HeroSection />
+    </>
   );
 }
 
