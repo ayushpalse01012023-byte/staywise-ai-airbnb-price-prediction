703c9250f75c2c126042ed83ef6846ebffdd023c
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-22 13:53:06 +0530

    updated predict page

---
+++ frontend/src/pages/PredictPage.jsx
@@ -0,0 +1,9 @@
+function PredictPage() {
+  return (
+    <section className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-semibold">PredictPage placeholder</h1>
+    </section>
+  );
+}
+
+export default PredictPage;
