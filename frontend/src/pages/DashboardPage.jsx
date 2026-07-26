11d343154c57ecaaa8bb2a3f34fd2f714e505f2d
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-26 14:47:03 +0530

    added dashboard stats

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,13 +1,11 @@
 import DashboardHero from "../components/dashboard/DashboardHero";
-
-
-
+import DashboardStats from "../components/dashboard/DashboardStats";
 
 function DashboardPage() {
   return (
     <>
       <DashboardHero />
-      
+      <DashboardStats />
     </>
   );
 }
