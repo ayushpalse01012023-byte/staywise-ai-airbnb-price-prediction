32a7673b5560efe50e075a6378cbd2220774a432
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-26 12:54:55 +0530

    uupdated predict page

--- frontend/src/pages/PredictPage.jsx
+++ frontend/src/pages/PredictPage.jsx
@@ -1,11 +1,21 @@
 import PredictionHero from "../components/prediction/PredictionHero";
 import PredictionFormSection from "../components/prediction/PredictionFormSection";
+import PredictionResultSection from "../components/prediction/PredictionResultSection";
+
 
 function PredictPage() {
   return (
     <>
       <PredictionHero />
       <PredictionFormSection />
+      <PredictionResultSection
+        predictedPrice={109.84}
+        confidence={94.6}
+        predictionTime={0.18}
+        modelName="XGBoost Regressor"
+        recommendation="This listing appears competitively priced based on location, availability and historical Airbnb trends."
+        insight="Our AI analyzed location, room type, host activity, reviews, and availability to estimate this nightly price."
+      />
     </>
   );
 }
