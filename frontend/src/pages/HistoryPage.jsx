87c50791ea3cb1dabd3c1f6ee6bda153337ba57d
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-26 19:26:21 +0530

    updated history page

--- frontend/src/pages/HistoryPage.jsx
+++ frontend/src/pages/HistoryPage.jsx
@@ -1,8 +1,10 @@
+import HistoryHero from "../components/history/HistoryHero";
+
 function HistoryPage() {
   return (
-    <section className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-semibold">HistoryPage placeholder</h1>
-    </section>
+    <>
+      <HistoryHero />
+    </>
   );
 }
 
