501665bb492412ca56397ce201e4fc5fbbb3bc49
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-26 14:41:30 +0530

    updated dashboard page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,8 +1,14 @@
+import DashboardHero from "../components/dashboard/DashboardHero";
+
+
+
+
 function DashboardPage() {
   return (
-    <section className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-semibold">DashboardPage placeholder</h1>
-    </section>
+    <>
+      <DashboardHero />
+      
+    </>
   );
 }
 
