f61a8e30c4f216ad5d986e9b364a78b8296998ab
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-26 13:07:43 +0530

    updated poredict page

--- frontend/src/pages/PredictPage.jsx
+++ frontend/src/pages/PredictPage.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
+
 import PredictionHero from "../components/prediction/PredictionHero";
 import PredictionFormSection from "../components/prediction/PredictionFormSection";
 import PredictionResultSection from "../components/prediction/PredictionResultSection";
 
-
 function PredictPage() {
+  // Stores the prediction returned from FastAPI
+  const [prediction, setPrediction] = useState(null);
+
   return (
     <>
       <PredictionHero />
-      <PredictionFormSection />
-      <PredictionResultSection
-        predictedPrice={109.84}
-        confidence={94.6}
-        predictionTime={0.18}
-        modelName="XGBoost Regressor"
-        recommendation="This listing appears competitively priced based on location, availability and historical Airbnb trends."
-        insight="Our AI analyzed location, room type, host activity, reviews, and availability to estimate this nightly price."
+
+      <PredictionFormSection
+        prediction={prediction}
+        setPrediction={setPrediction}
       />
+
+      {/* Show result only after prediction is received */}
+      {prediction && (
+        <PredictionResultSection
+          predictedPrice={prediction.predicted_price}
+          confidence={94.6}
+          predictionTime={0.18}
+          modelName="XGBoost Regressor"
+          recommendation="This listing appears competitively priced based on location, availability, reviews, and historical Airbnb trends."
+          insight="Our AI analyzed the neighbourhood, room type, host activity, availability, location, and review patterns to estimate this nightly Airbnb price."
+        />
+      )}
     </>
   );
 }
