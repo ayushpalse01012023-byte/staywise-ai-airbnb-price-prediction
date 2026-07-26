df1090aa20e6bf058127190013fd9cfe94a15d48
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-26 19:36:22 +0530

    added search and filter in history page

--- frontend/src/pages/HistoryPage.jsx
+++ frontend/src/pages/HistoryPage.jsx
@@ -1,11 +1,13 @@
 import HistoryHero from "../components/history/HistoryHero";
 import HistoryStats from "../components/history/HistoryStats";
+import SearchFilter from "../components/history/SearchFilter";
 
 function HistoryPage() {
   return (
     <>
       <HistoryHero />
       <HistoryStats />
+      <SearchFilter />
     </>
   );
 }
