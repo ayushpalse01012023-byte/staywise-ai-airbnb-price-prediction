f9daf53eecd9eacb3eee4effbb1632e0c48543ad
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-27 15:18:46 +0530

    updated model

--- backend/models.py
+++ backend/models.py
@@ -9,10 +9,15 @@ class Prediction(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
+    # Location
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+
     neighbourhood_group = Column(String, nullable=False)
     neighbourhood = Column(String, nullable=False)
     room_type = Column(String, nullable=False)
 
+    # Listing Features
     minimum_nights = Column(Integer, nullable=False)
     number_of_reviews = Column(Integer, nullable=False)
     reviews_per_month = Column(Float, nullable=False)
@@ -20,6 +25,12 @@ class Prediction(Base):
     calculated_host_listings_count = Column(Integer, nullable=False)
     availability_365 = Column(Integer, nullable=False)
 
+    # Review Information
+    review_year = Column(Integer, nullable=False)
+    review_month = Column(Integer, nullable=False)
+
+    # Model Output
     predicted_price = Column(Float, nullable=False)
 
+    # Timestamp
     created_at = Column(DateTime, default=datetime.utcnow)
