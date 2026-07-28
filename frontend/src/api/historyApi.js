bb1a689fc5857b466ef2562797c514ef33f51771
Ayush Palse <ayushpalse01012023@gmail.com>
2026-07-29 01:44:33 +0530

    updated history api

--- frontend/src/api/historyApi.js
+++ frontend/src/api/historyApi.js
@@ -4,3 +4,8 @@ export async function getPredictionHistory() {
   const response = await api.get("/history");
   return response.data;
 }
+
+export async function clearPredictionHistory() {
+  const response = await api.delete("/history");
+  return response.data;
+}
